8aef7c8008e74d4b467c1e17e357506b14c6ec11
Aksel Christoffer Wiig <aksel.christoffer.wiig@gmail.com>
2019-05-31 13:19:56 +0200

    Prints game board to console and finds living cells

--- game_of_life.py
+++ game_of_life.py
@@ -5,25 +5,31 @@ def create_board(x, y):
     return [[0 for i in range(x)] for j in range(y)]
 
 
-def update_board(board, cell):
-    # for i in board:
-    #    print(i)
-    #    for j in i:
-    #        print(j)
+def living_cells(board):
+    cells = []
 
-    return board
+    for i, val in enumerate(board):
+        print("Index: {}, Value: {}".format(i, val))
+        for j, value in enumerate(val):
+            if board[j][i] == 1:
+                cells.append([j, i])
+                #print("X index: {}, Y index: {}".format(j, i))
+
+    return cells
 
 
 def start_game():
-    board_size_x = 5  # int(input("Horizontal size(X): "))
-    board_size_y = 5  # int(input("Vertical size(Y): "))
+    board_size_x = 5
+    board_size_y = 5
     board = create_board(board_size_x, board_size_y)
 
     board[3][3] = 1
-    board[3][2] = 1
-    board[3][1] = 1
+    board[2][3] = 1
+    board[1][3] = 1
 
-    return update_board(board)
+    print(pd.DataFrame(board))
+
+    return living_cells(board)
 
 
-print(pd.DataFrame(start_game()))
+print(start_game())
